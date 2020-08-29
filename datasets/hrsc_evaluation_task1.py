4a772603dbb9790d61fbba2fe62d7633b05ffc33
Jingru Yi <jy486@krete.cs.rutgers.edu>
2020-08-29 12:00:11 -0400

    add

---
+++ datasets/hrsc_evaluation_task1.py
@@ -0,0 +1,264 @@
+# --------------------------------------------------------
+# dota_evaluation_task1
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jian Ding, based on code from Bharath Hariharan
+# Modified version
+# --------------------------------------------------------
+
+"""
+    To use the code, users should to config detpath, annopath and imagesetfile
+    detpath is the path for 15 result files, for the format, you can refer to "http://captain.whu.edu.cn/DOTAweb/tasks.html"
+    search for PATH_TO_BE_CONFIGURED to config the paths
+    Note, the evaluation is on the large scale images
+"""
+import xml.etree.ElementTree as ET
+import os
+#import cPickle
+import numpy as np
+import matplotlib.pyplot as plt
+from . import polyiou
+from functools import partial
+import cv2
+
+def parse_gt(filename):
+    objects = []
+    target = ET.parse(filename).getroot()
+    for obj in target.iter('HRSC_Object'):
+        object_struct = {}
+        difficult = int(obj.find('difficult').text)
+        box_xmin = int(obj.find('box_xmin').text)  # bbox
+        box_ymin = int(obj.find('box_ymin').text)
+        box_xmax = int(obj.find('box_xmax').text)
+        box_ymax = int(obj.find('box_ymax').text)
+        mbox_cx = float(obj.find('mbox_cx').text)  # rbox
+        mbox_cy = float(obj.find('mbox_cy').text)
+        mbox_w = float(obj.find('mbox_w').text)
+        mbox_h = float(obj.find('mbox_h').text)
+        mbox_ang = float(obj.find('mbox_ang').text)*180/np.pi
+        rect = ((mbox_cx, mbox_cy), (mbox_w, mbox_h), mbox_ang)
+        pts_4 = cv2.boxPoints(rect)  # 4 x 2
+        bl = pts_4[0,:]
+        tl = pts_4[1,:]
+        tr = pts_4[2,:]
+        br = pts_4[3,:]
+        object_struct['name'] = 'ship'
+        object_struct['difficult'] = difficult
+        object_struct['bbox'] = [float(tl[0]),
+                                 float(tl[1]),
+                                 float(tr[0]),
+                                 float(tr[1]),
+                                 float(br[0]),
+                                 float(br[1]),
+                                 float(bl[0]),
+                                 float(bl[1])]
+        objects.append(object_struct)
+    return objects
+
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def voc_eval(detpath,
+             annopath,
+             imagesetfile,
+             classname,
+             ovthresh=0.5,
+             use_07_metric=False):
+
+    with open(imagesetfile, 'r') as f:
+        lines = f.readlines()
+    imagenames = [x.strip() for x in lines]
+    recs = {}
+    for i, imagename in enumerate(imagenames):
+        recs[imagename] = parse_gt(os.path.join(annopath.format(imagename)))
+    # extract gt objects for this class
+    class_recs = {}
+    npos = 0
+    for imagename in imagenames:
+        R = [obj for obj in recs[imagename] if obj['name'] == classname]
+        bbox = np.array([x['bbox'] for x in R])
+        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        det = [False] * len(R)
+        npos = npos + sum(~difficult)
+        class_recs[imagename] = {'bbox': bbox,
+                                 'difficult': difficult,
+                                 'det': det}
+
+    # read dets from Task1* files
+    detfile = detpath.format(classname)
+    with open(detfile, 'r') as f:
+        lines = f.readlines()
+
+    splitlines = [x.strip().split(' ') for x in lines]
+    image_ids = [x[0] for x in splitlines]
+    confidence = np.array([float(x[1]) for x in splitlines])
+
+    #print('check confidence: ', confidence)
+
+    BB = np.array([[float(z) for z in x[2:]] for x in splitlines])
+
+    if len(confidence)>1:
+        # sort by confidence
+        sorted_ind = np.argsort(-confidence)
+        sorted_scores = np.sort(-confidence)
+
+        #print('check sorted_scores: ', sorted_scores)
+        #print('check sorted_ind: ', sorted_ind)
+
+        ## note the usage only in numpy not for list
+        BB = BB[sorted_ind, :]
+        image_ids = [image_ids[x] for x in sorted_ind]
+    #print('check imge_ids: ', image_ids)
+    #print('imge_ids len:', len(image_ids))
+    # go down dets and mark TPs and FPs
+    nd = len(image_ids)
+    tp = np.zeros(nd)
+    fp = np.zeros(nd)
+    for d in range(nd):
+        R = class_recs[image_ids[d]]
+        bb = BB[d, :].astype(float)
+        ovmax = -np.inf
+        BBGT = R['bbox'].astype(float)
+
+        ## compute det bb with each BBGT
+
+        if BBGT.size > 0:
+            # compute overlaps
+            # intersection
+
+            # 1. calculate the overlaps between hbbs, if the iou between hbbs are 0, the iou between obbs are 0, too.
+            # pdb.set_trace()
+            BBGT_xmin =  np.min(BBGT[:, 0::2], axis=1)
+            BBGT_ymin = np.min(BBGT[:, 1::2], axis=1)
+            BBGT_xmax = np.max(BBGT[:, 0::2], axis=1)
+            BBGT_ymax = np.max(BBGT[:, 1::2], axis=1)
+            bb_xmin = np.min(bb[0::2])
+            bb_ymin = np.min(bb[1::2])
+            bb_xmax = np.max(bb[0::2])
+            bb_ymax = np.max(bb[1::2])
+
+            ixmin = np.maximum(BBGT_xmin, bb_xmin)
+            iymin = np.maximum(BBGT_ymin, bb_ymin)
+            ixmax = np.minimum(BBGT_xmax, bb_xmax)
+            iymax = np.minimum(BBGT_ymax, bb_ymax)
+            iw = np.maximum(ixmax - ixmin + 1., 0.)
+            ih = np.maximum(iymax - iymin + 1., 0.)
+            inters = iw * ih
+
+            # union
+            uni = ((bb_xmax - bb_xmin + 1.) * (bb_ymax - bb_ymin + 1.) +
+                   (BBGT_xmax - BBGT_xmin + 1.) *
+                   (BBGT_ymax - BBGT_ymin + 1.) - inters)
+
+            overlaps = inters / uni
+
+            BBGT_keep_mask = overlaps > 0
+            BBGT_keep = BBGT[BBGT_keep_mask, :]
+            BBGT_keep_index = np.where(overlaps > 0)[0]
+            # pdb.set_trace()
+            def calcoverlaps(BBGT_keep, bb):
+                overlaps = []
+                for index, GT in enumerate(BBGT_keep):
+
+                    overlap = polyiou.iou_poly(polyiou.VectorDouble(BBGT_keep[index]), polyiou.VectorDouble(bb))
+                    overlaps.append(overlap)
+                return overlaps
+            if len(BBGT_keep) > 0:
+                overlaps = calcoverlaps(BBGT_keep, bb)
+
+                ovmax = np.max(overlaps)
+                jmax = np.argmax(overlaps)
+                # pdb.set_trace()
+                jmax = BBGT_keep_index[jmax]
+
+        if ovmax > ovthresh:
+            if not R['difficult'][jmax]:
+                if not R['det'][jmax]:
+                    tp[d] = 1.
+                    R['det'][jmax] = 1
+                else:
+                    fp[d] = 1.
+        else:
+            fp[d] = 1.
+
+    # compute precision recall
+
+    # print('check fp:', fp)
+    # print('check tp', tp)
+
+
+    # print('npos num:', npos)
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+
+    rec = tp / float(npos)
+    # avoid divide by zero in case the first detection matches a difficult
+    # ground truth
+    prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+    ap = voc_ap(rec, prec, use_07_metric)
+
+    return rec, prec, ap
+
+def main():
+    detpath = r'PATH_TO_BE_CONFIGURED/Task1_{:s}.txt'
+    annopath = r'PATH_TO_BE_CONFIGURED/{:s}.txt' # change the directory to the path of val/labelTxt, if you want to do evaluation on the valset
+    imagesetfile = r'PATH_TO_BE_CONFIGURED/test.txt'
+    classnames = ['ship']
+    classaps = []
+    map = 0
+    for classname in classnames:
+        print('classname:', classname)
+        rec, prec, ap = voc_eval(detpath,
+                                 annopath,
+                                 imagesetfile,
+                                 classname,
+                                 ovthresh=0.5,
+                                 use_07_metric=True)
+        map = map + ap
+        #print('rec: ', rec, 'prec: ', prec, 'ap: ', ap)
+        print('ap: ', ap)
+        classaps.append(ap)
+
+        # umcomment to show p-r curve of each category
+        # plt.figure(figsize=(8,4))
+        # plt.xlabel('recall')
+        # plt.ylabel('precision')
+        # plt.plot(rec, prec)
+       # plt.show()
+    map = map/len(classnames)
+    print('map:', map)
+    classaps = 100*np.array(classaps)
+    print('classaps: ', classaps)
+
+if __name__ == '__main__':
+    main()
